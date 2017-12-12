ef46831bf29f676ebc466fafc85d04a885d3e76d
Luke Finney <lucaswfinney@gmail.com>
2017-12-12 15:56:03 -0700

    Add GET request for all a discussions comments

--- server.js
+++ server.js
@@ -53,6 +53,16 @@ app.get('/api/v1/discussions/:id', (request, response) => {
   .catch(error => response.status(500).json({ error }));
 });
 
+app.get('/api/v1/discussions/:id/comments', (request, response) => {
+  const { id } = request.params;
+
+  database('comments').where('discussionId', id).select()
+  .then((comments) => {
+    return response.status(200).json(comments);
+  })
+  .catch(error => response.status(500).json({ error }));
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
