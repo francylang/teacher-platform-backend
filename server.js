fe86013a3c150596c57d7908a87015c75c6bdf4b
Luke Finney <lucaswfinney@gmail.com>
2017-12-12 15:38:56 -0700

    Add GET request for Topic Tags and fixed typo

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,6 +15,14 @@ app.locals.title = 'Yo Teach';
 
 app.get('/', (request, response) => {
   response.send('Oh, hai!');
+});
+
+app.get('/api/v1/topicTags', (request, response) => {
+  database('topicTags').select()
+  .then((topicTags) => {
+    return response.status(200).json(topicTags);
+  })
+  .catch(error => response.status(500).json({ error }));
 });
 
 app.listen(app.get('port'), () => {
