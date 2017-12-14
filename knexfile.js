ed2f7a451e0510a9e6799680acff12c7d28e96bf
Luke Finney <lucaswfinney@gmail.com>
2017-12-14 15:33:54 -0700

    Setup Heroku

--- knexfile.js
+++ knexfile.js
@@ -22,4 +22,12 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+  },
 };
