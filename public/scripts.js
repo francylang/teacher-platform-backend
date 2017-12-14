7483791c14d34e84a1d27fba8f88302688f0957c
Luke Finney <lucaswfinney@gmail.com>
2017-12-14 14:02:02 -0700

    Fixed regex expression

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
 const verifyEmail = (email) => {
-  const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  const emailPattern = /^[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
   return email.match(emailPattern);
 };
 
